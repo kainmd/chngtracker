d4d57cb0bdb97fe02114d5c12593c74d0c4710ec
kainmd <70366212+kainmd@users.noreply.github.com>
2023-12-19 15:50:48 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,70 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Threading;
 
 namespace MyNamespace
 {
-    class MyClassCS
+    class NewClassCS
     {
-        // Path for the log file
         private static string logFilePath;
+        private static DateTime lastLogFileCreationTime;
 
-        // List to store pending changes
-        private static List<string> pendingChanges = new List<string>();
-
-        // Timer for logging pending changes
-        private static Timer timer;
-
-        // Flag to enable or disable silent mode
-        private static bool SilentMode = true;
-
-        // Main entry point of the application
         static void Main()
         {
-            Console.WriteLine("Enter the path of the directory to monitor:");
-            string directoryPath = Console.ReadLine();
+            Console.WriteLine("Enter the path to the folder you want to monitor:");
+            string folderPath = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(directoryPath) || !Directory.Exists(directoryPath))
+            // Validate folder path
+            if (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
             {
-                Console.WriteLine("Invalid directory path. Exiting.");
-                Environment.Exit(0);
+                Console.WriteLine("Invalid folder path. Exiting program.");
+                return;
             }
 
-            Console.WriteLine("Enter the path of the log file:");
-            logFilePath = Console.ReadLine();
+            Console.WriteLine("Enter the path for the log file (or press Enter to use default 'log.txt'):");
+            string userInput = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(logFilePath))
+            // Set default log file path if user input is empty or whitespace
+            logFilePath = string.IsNullOrWhiteSpace(userInput) ? "c:\\files\\logs.txt" : userInput.Trim();
+
+            // Validate the log file path
+            if (Path.GetInvalidFileNameChars().Any(c => logFilePath.Contains(c)))
             {
                 Console.WriteLine("Invalid log file path. Exiting.");
-                Environment.Exit(0);
+                return;
             }
 
-            // Hide the current console window
-            var currentProcess = Process.GetCurrentProcess();
-            NativeMethods.ShowWindow(currentProcess.MainWindowHandle, NativeMethods.SW_HIDE);
+            // Initialize lastLogFileCreationTime with the current date and time
+            lastLogFileCreationTime = DateTime.Now;
 
-            // Create a new console window
-            var newProcess = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "cmd.exe",
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                }
-            };
+            using var watcher = new FileSystemWatcher(folderPath);
 
-            newProcess.Start();
-
-            // Create a FileSystemWatcher to monitor changes in the specified directory
-            using var watcher = new FileSystemWatcher(directoryPath);
-
-            // Specify the types of changes to monitor
             watcher.NotifyFilter = NotifyFilters.Attributes
                                  | NotifyFilters.CreationTime
                                  | NotifyFilters.DirectoryName
@@ -74,138 +47,98 @@ namespace MyNamespace
                                  | NotifyFilters.Security
                                  | NotifyFilters.Size;
 
-            // Watch all files in subdirectories
-            watcher.IncludeSubdirectories = true;
-
-            // Attach event handlers for various file system events
             watcher.Created += OnCreated;
             watcher.Deleted += OnDeleted;
             watcher.Renamed += OnRenamed;
             watcher.Error += OnError;
-
-            // Enable the FileSystemWatcher to begin monitoring
+            
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true; // Monitor subdirectories
             watcher.EnableRaisingEvents = true;
 
-            // Initialize timer for logging pending changes every second
-            timer = new Timer(LogPendingChanges, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
-
-            // Schedule the deletion of the log file after 30 days
-            Timer deleteTimer = new Timer(DeleteLogFile, null, TimeSpan.FromDays(30), Timeout.InfiniteTimeSpan);
-
-            // Display a message and wait for user input to exit the application
+            Console.WriteLine($"Monitoring folder: {folderPath}");
+            Console.WriteLine($"Logging changes to: {logFilePath}");
             Console.WriteLine("Press enter to exit.");
-            Console.ReadLine();
 
-            // Exit the new console window
-            newProcess.StandardInput.WriteLine("exit");
-            newProcess.WaitForExit();
+            Console.ReadLine(); // Wait for user input
 
-            // Show the original console window
-            NativeMethods.ShowWindow(currentProcess.MainWindowHandle, NativeMethods.SW_SHOW);
-        }
-
-        // Event handler for file creation and deletion
-        private static void OnCreated(object sender, FileSystemEventArgs e)
-        {
-            AddPendingChange(e.ChangeType.ToString(), e.FullPath);
-        }
-
-        // Event handler for file deletion
-        private static void OnDeleted(object sender, FileSystemEventArgs e)
-        {
-            AddPendingChange(e.ChangeType.ToString(), e.FullPath);
-        }
-
-        // Event handler for file renaming
-        private static void OnRenamed(object sender, RenamedEventArgs e)
-        {
-            AddPendingChange(e.ChangeType.ToString(), $"Old: {e.OldFullPath}, New: {e.FullPath}");
-        }
-
-        // Event handler for FileSystemWatcher errors
-        private static void OnError(object sender, ErrorEventArgs e)
-        {
-            AddPendingChange("Error", e.GetException()?.Message);
-        }
-
-        // Add a change to the pending changes list
-        private static void AddPendingChange(string changeType, string details)
-        {
-            string logMessage = $"{DateTime.Now} - {changeType}: {details} by {Environment.UserName}";
-            pendingChanges.Add(logMessage);
-        }
-
-        // Log pending changes to the specified log file
-        private static void LogPendingChanges(object state)
-        {
-            if (pendingChanges.Count > 0)
+            // Redirect console output to the log file
+            using (var logStream = new StreamWriter(logFilePath, append: true))
             {
-                if (!SilentMode)
-                {
-                    Console.WriteLine($"Logging {pendingChanges.Count} changes...");
-                }
+                Console.SetOut(logStream);
 
-                try
-                {
-                    using (StreamWriter writer = new StreamWriter(logFilePath, true))
-                    {
-                        foreach (string change in pendingChanges)
-                        {
-                            writer.WriteLine(change);
-                        }
-                    }
-                    pendingChanges.Clear();
-                }
-                catch (Exception ex)
-                {
-                    if (!SilentMode)
-                    {
-                        Console.WriteLine($"Error writing to log file: {ex.Message}");
-                    }
-                }
+                Console.ReadLine(); // Wait for user input
+
+                // Restore standard output
+                Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
             }
         }
 
-        // Delete the log file if it exists and is older than 30 days
-        private static void DeleteLogFile(object state)
+        private static void OnCreated(object sender, FileSystemEventArgs e)
         {
+            Console.WriteLine($"Created: {e.FullPath}");
+
+            // Check if 30 days have passed since the last log file creation
+            if ((DateTime.Now - lastLogFileCreationTime).TotalDays >= 30)
+            {
+                // Create a new log file
+                CreateNewLogFile();
+            }
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Deleted: {e.FullPath}");
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void CreateNewLogFile()
+        {
+            // Create a backup of the current log file with a timestamp in the filename
+            string backupFileName = $"log_backup_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+            string backupFilePath = Path.Combine(Path.GetDirectoryName(logFilePath), backupFileName);
+
             try
             {
-                // Check if the log file exists
-                if (File.Exists(logFilePath))
-                {
-                    // Get the creation time of the file
-                    DateTime creationTime = File.GetCreationTime(logFilePath);
+                // Rename the current log file to the backup file
+                File.Move(logFilePath, backupFilePath);
 
-                    // Check if the file is older than 30 days
-                    if (DateTime.Now - creationTime > TimeSpan.FromDays(30))
-                    {
-                        // Delete the file
-                        File.Delete(logFilePath);
-                        if (!SilentMode)
-                        {
-                            Console.WriteLine($"Log file deleted. Path: {logFilePath}");
-                        }
-                    }
+                // Create a new log file
+                using (var logStream = new StreamWriter(logFilePath, append: true))
+                {
+                    Console.SetOut(logStream);
                 }
+
+                // Update lastLogFileCreationTime with the current date and time
+                lastLogFileCreationTime = DateTime.Now;
+
+                Console.WriteLine($"New log file created: {logFilePath}");
             }
             catch (Exception ex)
             {
-                if (!SilentMode)
-                {
-                    Console.WriteLine($"Error deleting log file: {ex.Message}");
-                }
+                Console.WriteLine($"Error creating new log file: {ex.Message}");
             }
         }
 
-        // NativeMethods class for interop with user32.dll
-        private static class NativeMethods
+        private static void PrintException(Exception? ex)
         {
-            public const int SW_HIDE = 0;
-            public const int SW_SHOW = 5;
-
-            [System.Runtime.InteropServices.DllImport("user32.dll")]
-            public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
         }
     }
 }
